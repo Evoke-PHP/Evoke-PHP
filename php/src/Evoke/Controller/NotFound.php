fca8d38b342e35c55b0f64d0adfd1d5c7d0fbd61
Paul Young <evoke@youngish.homelinux.org>
2012-05-09 13:20:36 +0930

    Not Found controller updated as per Error controller.

--- php/src/Evoke/Controller/NotFound.php
+++ php/src/Evoke/Controller/NotFound.php
@@ -5,45 +5,19 @@ use Evoke\Iface;
 
 class NotFound extends \Evoke\Controller
 {
-	/** Construct the Not Found response.
-	 *  @param provider @object Provider
-	 *  @param request  @object Request
-	 *  @param response @object Response
-	 *  @param mediaTypeRouterFactory @object Media Type Router Factory.
-	 *  @param defaults               @array  Defaults for the content type and
-	 *                                        output format.
-	 *  @param pageSetup              @array  Setup for page based output
-	 *                                        formats.
-	 */
-	public function __construct(
-		Iface\Provider      		  	   $provider,
-		Iface\HTTP\Request  		  	   $request,
-		Iface\HTTP\Response 		  	   $response,
-		Iface\HTTP\MediaType\RouterFactory $mediaTypeRouterFactory,
-		Array                              $defaults  = array(
-			'Content_Type'  => 'application/xhtml+xml',
-			'Output_Format' => 'XHTML'),
-		Array                              $pageSetup = array())
-	{
-		// Not Found has no parameters, so use an empty array for it.
-		parent::__construct(array(), $provider, $request, $response,
-		                    $mediaTypeRouterFactory, $defaults, $pageSetup);
-	}
-
 	/*********************/
 	/* Protected Methods */
 	/*********************/
 
-	protected function html5ALL(Iface\Writer\Page $writer)
+	protected function html5ALL()
 	{
-		$this->xhtmlALL($writer);
+		$this->xhtmlALL();
 	}
 	
-	protected function jsonALL(Iface\Writer $writer)
+	protected function jsonALL()
 	{
-		$writer->write(
-			array('Code' => '404',
-			      'Text' => 'Not Found'));
+		$this->writer->write(array('Code' => '404',
+		                           'Text' => 'Not Found'));
 	}
 	
 	/** Respond with the error code first.
@@ -51,29 +25,33 @@ class NotFound extends \Evoke\Controller
 	 */
 	protected function respond($outputFormat)
 	{
-		$this->response->setResponseCode(404);
+		if (!headers_sent())
+		{
+			$this->response->setResponseCode(404);
+		}
+		
 		parent::respond($outputFormat);
 	}
 
-	protected function textALL(Iface\Writer $writer)
+	protected function textALL()
 	{
-		$writer->write('404 Not Found');
+		$this->writer->write('404 Not Found');
 	}
 	
-	protected function xhtmlALL(Iface\Writer\Page $writer)
+	protected function xhtmlALL()
 	{
 		
-		$writer->writeStart(
+		$this->writer->writeStart(
 			array_merge($this->pageSetup,
 			            array('Description' => 'Not Found',
 			                  'Title'       => 'Page Not Found')));
-		$this->writeMessageBoxXML($writer);
-		$writer->writeEnd();
+		$this->writeXMLNotFound();
+		$this->writer->writeEnd();
 	}
 	
-	protected function xmlALL(Iface\Writer $writer)
+	protected function xmlALL()
 	{
-		$this->writeMessageBoxXML($writer);
+		$this->writeXMLNotFound();
 	}
 
 	/*******************/
@@ -81,16 +59,17 @@ class NotFound extends \Evoke\Controller
 	/*******************/
 
 	/// Write a Message Box in XML showing the Not Found message.
-	private function writeMessageBoxXML(Iface\Writer $writer)
+	private function writeXMLNotFound()
 	{
 		$element = $this->provider->make(
 			'Evoke\Element\Message\Box',
 			array('Attribs' => array('class' => 'Message_Box System')));
+
+		$this->params += array(
+			'Description' => 'The requested page could not be found.',
+			'Title'       => 'Not Found');
 		
-		$writer->write(
-			$element->set(
-				array('Description' => 'The requested page could not be found.',
-				      'Title'       => 'Not Found')));
+		$this->writer->write($element->set($this->params));
 	}	
 }
 // EOF
