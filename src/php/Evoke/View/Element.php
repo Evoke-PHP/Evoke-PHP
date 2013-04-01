45bfa1a7ae5859c9875693cbb9de7c73ac678ee2
Paul Young <evoke@youngish.homelinux.org>
2013-04-01 17:19:17 +1030

    Possible View\Element implementation.

--- src/php/Evoke/View/Element.php
+++ src/php/Evoke/View/Element.php
@@ -6,22 +6,35 @@ use InvalidArgumentException;
 /**
  * An XHTML element view.
  */
-class Element implements ViewIface
+class Element extends View
 {
-	/** Tag
-	 * @var @string
-	 */
-	protected $tag;
+	protected
+		/** A map of attributes that should be retrieved from the get
+		 *  parameters.
+		 *  @var string[]
+		 */
+		$attribMap,
+		
+		/** Attributes for the element.
+		 * @var string[]
+		 */
+		$attribs,
 
-	/** Attribs
-	 * @var @array
-	 */
-	protected $attribs;
+		/** Children
+		 * @var mixed
+		 */
+		$children,
 
-	/** Children
-	 * @var @object
-	 */
-	protected $children;
+		/** Tag
+		 * @var string
+		 */
+		$tag,
+
+		/** Map of the position of the text items that are to be obtained from
+		 *  the get parameters to the parameter that should be used.
+		 *  @var string[]
+		 */
+		$textMap;
 
 	/**
 	 * Construct a simple XHTML element view.
@@ -36,12 +49,27 @@ class Element implements ViewIface
 	{
 		if (!is_string($tag))
 		{
-			throw new InvalidArgumentException('requires tag as string');
+			throw new InvalidArgumentException('needs tag as string');
 		}
 
-		$this->tag      = $tag;
-		$this->attribs  = $attribs;
-		$this->children = $children;
+		$this->attribMap = array();
+		$this->attribs   = $attribs;
+
+		if (!isset($children))
+		{
+			$this->children = array();
+		}
+		elseif (is_string($children))
+		{
+			$this->children = array($children);
+		}
+		else
+		{
+			$this->children = $children;
+		}
+		
+		$this->tag       = $tag;
+		$this->textMap   = array();
 	}
 
 	/******************/
@@ -49,16 +77,74 @@ class Element implements ViewIface
 	/******************/
 
 	/**
-	 * Get the view (of the data) to be written.
+	 * Add an attribute to be used from the get parameters.
 	 *
-	 * @param mixed   The data for the view.
-	 * @param mixed[] Parameters for retrieving the view.
+	 * @param string The attribute to add.
+	 * @param string The parameter to use for the value of the attribute.
+	 */
+	public function addAttrib(/* String */ $attrib,
+	                          /* String */ $value,
+	                          /* String */ $separator = ' ')
+	{
+		
+		$this->attribMap[$attrib] = $param;
+	}
+
+	/**
+	 * Add an attribute to be used from the get parameters.
+	 *
+	 * @param string The attribute to add.
+	 * @param string The parameter to use for the value of the attribute.
+	 */
+	public function addAttribParam(/* String */ $attrib, /* String */ $param)
+	{
+		$this->attribMap[$attrib] = $param;
+	}
+	
+	/**
+	 * Add a child element (text or element array).
+	 *
+	 * @param mixed The child to add.
+	 */
+	public function addChild(/* Mixed */ $child)
+	{
+		$this->children[] = $child;
+	}
+
+	/**
+	 * Add some text to be used from the get parameters.
+	 *
+	 * @param string The parameter to be used for the text.
+	 */
+	public function addTextParam(/* String */ $param)
+	{
+		// No need to -1 as we add the placeholder afterwards.
+		$this->textMap[count($this->children)] = $param;
+		$this->children[] = '';
+	}
+	
+	/**
+	 * Get the view (of the data) to be written.
 	 *
 	 * @return mixed[] The view data.
 	 */
-	public function get($data = NULL, Array $params = array())
+	public function get()
 	{
-		return array($this->tag, $this->attribs, $this->children);
+		$attribs = $this->attribs;
+		
+		foreach ($this->attribMap as $attrib => $param)
+		{
+			$attribs[$attrib] .= ' ' . $params[$param];
+		}
+
+		$children = $this->children;
+		
+		foreach ($this->textMap as $index => $param)
+		{
+			$children[$index] = $params[$param];
+		}
+		
+		return array($this->tag, $attribs, $children);
 	}
 }
 // EOF
