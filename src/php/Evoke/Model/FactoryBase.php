f7d53d4a328e1a064af52d1e2d93c16d39836935
Paul Young <evoke@youngish.homelinux.org>
2013-06-20 22:06:46 +0930

    Refactor Model Factory Base.

--- src/php/Evoke/Model/FactoryBase.php
+++ src/php/Evoke/Model/FactoryBase.php
@@ -9,16 +9,9 @@ namespace Evoke\Model;
 use DomainException,
 	Evoke\Model\Data\Data,
 	Evoke\Model\Data\RecordList,
-	Evoke\Model\Mapper\DB\Joint,
 	Evoke\Model\Mapper\DB\Table,
 	Evoke\Model\Mapper\DB\Tables,
-	Evoke\Model\Mapper\Session as MapperSession,
-	Evoke\Persistence\DB\SQLIface,
-	Evoke\Persistence\DB\Table\Info,
-	Evoke\Persistence\DB\Table\Joins,
-	Evoke\Persistence\DB\Table\ListID,
-	Evoke\Persistence\Session,
-	Evoke\Persistence\SessionManager;
+	Evoke\Model\Mapper\Session as MapperSession;
 
 /**
  * Model Factory Base
@@ -34,22 +27,6 @@ use DomainException,
  */
 abstract class FactoryBase
 {
-	/**
-	 * SQL Object.
-	 * @var Evoke\Persistence\DB\SQLIface
-	 */
-	protected $sql;
-
-	/**
-	 * Construct the model factory.
-	 *
-	 * @param SQLIface SQL object for DB based models.
-	 */
-	public function __construct(SQLIface $sql)
-	{
-		$this->sql = $sql;
-	}
-
 	/*********************/
 	/* Protected Methods */
 	/*********************/
@@ -116,56 +93,6 @@ abstract class FactoryBase
 
 		return new $dataType(array(), $builtData);
 	}
-	
-	/**
-	 * Create all of the joins using an associative array of table joins.  The
-	 * keys of the array represent the table names.  The value for each table
-	 * is a string that specifies the joins from the table as a string using
-	 * the following grammar:
-	 * 
-	 *     Parent_Field  <Parent field name for the Join>
-	 *     Child_Field   <Child field name for the join>
-	 *     Child_Table   <Table name for the child field that is being joint>
-	 *     Join          Parent_Field=Child_Table.Child_Field
-	 *     Table_Joins   Join(,Join)*
-	 *
-	 * @param mixed[] The joins to be built.
-	 * @param string  The initial table name to start the joins from.
-	 */
-	protected function createJoins(Array $joins, $tableName)
-	{
-		if (!isset($joins[$tableName]))
-		{
-			return array();
-		}
-
-		$tableJoins = explode(',', $joins[$tableName]);
-		$builtJoins = array();
-		$pattern =
-			'(^(?<Parent_Field>\w+)=(?<Child_Table>\w+)\.(?<Child_Field>\w+)$)';
-		
-		foreach ($tableJoins as $index => $tableJoin)
-		{
-			if (!preg_match($pattern, $tableJoin, $matches))
-			{
-				throw new DomainException(
-					__METHOD__ . var_export($tableJoin, true) .
-					' join for table: ' . $tableName . ' at index: ' . $index .
-					' is not valid.');
-			}
-			else
-			{
-				$builtJoins[] = new Joins(
-					new Info($this->sql, $matches['Child_Table']),
-					$matches['Child_Table'],
-					$matches['Parent_Field'],
-					$matches['Child_Field'],
-					$this->createJoins($joins, $matches['Child_Table']));
-			}
-		}
-
-		return $builtJoins;
-	}
 
 	/**
 	 * Create a mapper that maps a menu from the DB.
@@ -183,31 +110,6 @@ abstract class FactoryBase
 			      'Fields'     => '*',
 			      'Order'      => 'Menu_List_T_Lft ASC',
 			      'Limit'      => 0));
-	}
-
-	/**
-	 * Create a mapper that maps a joint set of data from the DB.
-	 *
-	 * @param string   The name of the primary table.
-	 * @param string[] The joins for the data set.
-	 * @param mixed[]  The select statement settings.
-	 *
-	 * @return Evoke\Model\Mapper\DB\Joint
-	 */	 
-	protected function createMapperDBJoint(/* String */ $tableName,
-	                                       Array        $joins,
-	                                       Array        $select =  array())
-	{
-		return new Joint($this->sql,
-		                 $tableName,
-		                 new Joins(
-			                 new Info($this->sql, $tableName),
-			                 $tableName,
-			                 NULL,
-			                 NULL,
-			                 $this->createJoins($joins, $tableName)),
-		                 new ListID($this->sql),
-		                 $select);
 	}
 
 	/**
