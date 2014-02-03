f1cf0f9ad65275e1b4e477362299146cddc9b23c
Paul Young <evoke@youngish.homelinux.org>
2014-02-03 17:46:58 +1030

    Rework Processing before testing.

--- src/php/Evoke/Service/Processing.php
+++ src/php/Evoke/Service/Processing.php
@@ -23,15 +23,15 @@ class Processing implements ProcessingIface
 {
 	/**
 	 * Associative array of request IDs to processing callback.
-	 * @var mixed[]
+	 * @var callable[]
 	 */
-	protected $callbacks;
+	protected $callbacks = array();
 
 	/**
 	 * The data that we are processing.
 	 * @var mixed[]
 	 */
-	protected $data;
+	protected $data = array();
 
 	/**
 	 * Whether a key is required to match for processing.
@@ -43,89 +43,78 @@ class Processing implements ProcessingIface
 	 * Whether only a single request type can be processed at a time.
 	 * @var bool
 	 */
-	protected $uniqueMatch;
+	protected $uniqueMatchRequired;
 
 	/**
 	 * Construct a Processing object.
 	 *
-	 * @param mixed[] Associative array of data to process.
-	 * @param mixed[] Processing keys to callback lists.
-	 * @param bool    Whether a match is required.
-	 * @param bool    Whether a unique match is required.
+	 * @param bool Whether a match is required.
+	 * @param bool Whether a unique match is required.
 	 */
-	public function __construct(Array        $data,
-	                            Array        $callbacks,
-	                            /* Bool   */ $matchRequired = false,
-	                            /* Bool   */ $uniqueMatch   = true)
+	public function __construct(/* Bool */ $matchRequired       = false,
+	                            /* Bool */ $uniqueMatchRequired = true)
 	{
-		$this->callbacks     = $callbacks;
-		$this->data          = $data;
-		$this->matchRequired = $matchRequired;
-		$this->uniqueMatch   = $uniqueMatch;
+		$this->matchRequired       = $matchRequired;
+		$this->uniqueMatchRequired = $uniqueMatchRequired;
 	}
 
 	/******************/
 	/* Public Methods */
 	/******************/
 
+	public function appendCallback(/* String */ $processingKey,
+	                               callable     $callback)
+	{
+		$this->callbacks[$processingKey][] = $callback;
+	}
+	
 	/**
 	 * Process the request.
 	 */
 	public function process()
 	{
-		$matchedCallbacks = $this->getMatchedCallbacks();
-		
-		if ($this->matchRequired && (count($matchedCallbacks) === 0))
+		$matchedKeys = array_intersect_key($this->callbacks, $this->data);
+	
+		if ($this->matchRequired && (count($matchedKeys) === 0))
 		{
-			trigger_error(
-				'Match_Required for Request_Keys: ' .
-				var_export(array_keys($this->requestKeys), true) .
-				' with request data: ' . var_export($requestData, true),
-				E_USER_WARNING);
-			return;
+			throw new DomainException(
+				'Match_Required processing request with keys: ' .
+				implode(' ', array_keys($this->data)) .
+				' recognized keys are: ' .
+				implode(' ', array_keys($this->callbacks)));
 		}
-		elseif ($this->uniqueMatch && count($matchedCallbacks) > 1)
+		elseif ($this->uniqueMatchRequired && count($matchedKeys) > 1)
 		{
-			trigger_error(
-				'Unique_Match required for Request_Keys: ' .
-				var_export(array_keys($this->requestKeys)) .
-				' with request data: ' . var_export($requestData, true),
-				E_USER_WARNING);
-			return;
+			throw new DomainException(
+				'Unique match required processing request with keys: ' .
+				implode(' ', array_keys($this->data)) .
+				' recognized keys are: ' .
+				implode(' ', array_keys($this->callbacks)),
+				' matched keys are: ' .
+				implode(' ', $matchedKeys));
 		}
 
-		foreach ($matchedCallbacks as $key => $callbacks)
-		{
-			// The key that defines the callback should not be passed.
-			$callbackData = $this->data;
-			unset($callbackData[$key]);
-			
-			if (is_callable($callbacks))
-			{
-				call_user_func($callbacks, $callbackData);
-			}
-			else
-			{
-				foreach ($callbacks as $callback)
-				{
-					call_user_func($callback, $callbackData);
-				}
-			}
-		}
+		foreach ($matchedKeys as $key => $callbacks)
+        {
+            // The key that defines the callback should not be passed.
+            $callbackData = $this->data;
+            unset($callbackData[$key]);
+
+            foreach ($callbacks as $callback)
+            {
+	            call_user_func($callback, $callbackData);
+            }
+        }
 	}
 
-	/*********************/
-	/* Protected Methods */
-	/*********************/
-
 	/**
-	 * Get the matched callback lists for the data.
+	 * Set the data for the request that we are processing.
 	 *
-	 * @return mixed[] The callback lists that match the data.
+	 * @param mixed[] The request data that we are processing.
 	 */
-	protected function getMatchedCallbacks()
+	public function setData(Array $data)
 	{
-		return array_intersect_key($this->callbacks, $this->data);
+		$this->data = $data;
 	}
 }
 // EOF
--- src/php/Evoke/Service/ProcessingIface.php
+++ src/php/Evoke/Service/ProcessingIface.php
@@ -20,5 +20,12 @@ interface ProcessingIface
 	 * Process the request.
 	 */
 	public function process();
+
+	/**
+	 * Set the data for the request that we are processing.
+	 *
+	 * @param mixed[] The request data that we are processing.
+	 */
+	public function setData(Array $data);
 }
 // EOF
